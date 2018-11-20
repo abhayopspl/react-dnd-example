a26ec4c54e3407ef356afe89e02cd7c903a24a9d
Abhay Shiravanthe <35765637+abhayopspl@users.noreply.github.com>
2018-11-20 12:49:14 +0530

    Initial commit

---
+++ src/initialData.js
@@ -0,0 +1,28 @@
+export const initialData = {
+  tasks: {
+    "task-1": {
+      id: "task-1",
+      content: "Cleanse the world"
+    },
+    "task-1": {
+      id: "task-2",
+      content: "Juice the Jews"
+    },
+    "task-1": {
+      id: "task-3",
+      content: "Khazars are Aktimoshiv"
+    },
+    "task-1": {
+      id: "task-4",
+      content: "Need not wonder"
+    }
+  },
+  columns: {
+    "column-1": {
+      id: "column-1",
+      title: "To Do",
+      tasksIds: ["task-1", "task-2", "task-3", "task-4"]
+    }
+  },
+  columnOrder: ["column-1"]
+};
